43e40d1d40d1ceee67f16a51686bb1c2f3c0cc7d
Marcos <marcos.2h.oshiro@gmail.com>
2025-03-01 10:58:26 -0400

    feat: criação da tabela de usuarios

---
+++ internal/api/product_handlers.go
@@ -0,0 +1,7 @@
+package api
+
+import "net/http"
+
+func (api *Api) handleCreateProduct(w http.Response, r *http.Request) {
+	panic("TODO - NOT IMPLEMENTED")
+}
---
+++ internal/services/products_service.go
@@ -0,0 +1,18 @@
+package services
+
+import (
+	"github.com/MarcosHHOshiro/Gobid/internal/store/pgstore"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type ProductService struct {
+	pool    *pgxpool.Pool
+	queries *pgstore.Queries
+}
+
+func NewProductService(pool *pgxpool.Pool) ProductService {
+	return ProductService{
+		pool:    pool,
+		queries: pgstore.New(pool),
+	}
+}
