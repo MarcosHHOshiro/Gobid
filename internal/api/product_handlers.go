17aa346badf0de45351acacadc5a041aa42fa594
Marcos <marcos.2h.oshiro@gmail.com>
2025-03-03 19:33:48 -0400

    feat: implementando funcionalidade de cadastrar produto

--- internal/api/product_handlers.go
+++ internal/api/product_handlers.go
@@ -1,7 +1,45 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
 
-func (api *Api) handleCreateProduct(w http.Response, r *http.Request) {
-	panic("TODO - NOT IMPLEMENTED")
+	"github.com/MarcosHHOshiro/Gobid/internal/jsonutils"
+	"github.com/MarcosHHOshiro/Gobid/internal/usecase/product"
+	"github.com/google/uuid"
+)
+
+func (api *Api) handleCreateProduct(w http.ResponseWriter, r *http.Request) {
+	data, problems, err := jsonutils.DecodeValidJson[product.CreateProductReq](r)
+	if err != nil {
+		jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+		return
+	}
+
+	userId, ok := api.Sessions.Get(r.Context(), "AuthenticatedUserId").(uuid.UUID)
+	if !ok {
+		jsonutils.EncodeJson(w, r, http.StatusInternalServerError, map[string]any{
+			"error": "unexpected error, try again later",
+		})
+		return
+	}
+
+	id, err := api.ProductService.CreateProduct(
+		r.Context(),
+		userId,
+		data.ProductName,
+		data.Description,
+		data.Baseprice,
+		data.AuctionEnd,
+	)
+	if err != nil {
+		jsonutils.EncodeJson(w, r, http.StatusInternalServerError, map[string]any{
+			"error": "failed to create product auction, try again later",
+		})
+		return
+	}
+
+	jsonutils.EncodeJson(w, r, http.StatusCreated, map[string]any{
+		"message":    "product created with success",
+		"product_id": id,
+	})
 }
--- internal/services/products_service.go
+++ internal/services/products_service.go
@@ -1,7 +1,11 @@
 package services
 
 import (
+	"context"
+	"time"
+
 	"github.com/MarcosHHOshiro/Gobid/internal/store/pgstore"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -15,4 +19,26 @@ func NewProductService(pool *pgxpool.Pool) ProductService {
 		pool:    pool,
 		queries: pgstore.New(pool),
 	}
+}
+
+func (ps *ProductService) CreateProduct(
+	ctx context.Context,
+	sellerId uuid.UUID,
+	productName,
+	description string,
+	baseprice float64,
+	auctionEnd time.Time,
+) (uuid.UUID, error) {
+	id, err := ps.queries.CreateProduct(ctx, pgstore.CreateProductParams{
+		SellerID:    sellerId,
+		ProductName: productName,
+		Baseprice:   baseprice,
+		Description: description,
+		AuctionEnd:  auctionEnd,
+	})
+	if err != nil {
+
+		return uuid.UUID{}, err
+	}
+	return id, nil
 }
