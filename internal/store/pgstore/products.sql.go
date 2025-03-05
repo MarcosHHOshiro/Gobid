6c81867d9ed16c48ee88daccc6474fdf9aeb8614
Marcos <marcos.2h.oshiro@gmail.com>
2025-03-04 20:29:03 -0400

    feat: criação da tabela bids, implementação da funcionalidade de cadastro de bids

--- internal/store/pgstore/products.sql.go
+++ internal/store/pgstore/products.sql.go
@@ -40,3 +40,70 @@ func (q *Queries) CreateProduct(ctx context.Context, arg CreateProductParams) (u
 	err := row.Scan(&id)
 	return id, err
 }
+
+const getAllProducts = `-- name: GetAllProducts :many
+SELECT 
+    seller_id,
+    product_name, 
+    description, 
+    baseprice,
+    auction_end
+FROM 
+    products
+`
+
+type GetAllProductsRow struct {
+	SellerID    uuid.UUID `json:"seller_id"`
+	ProductName string    `json:"product_name"`
+	Description string    `json:"description"`
+	Baseprice   float64   `json:"baseprice"`
+	AuctionEnd  time.Time `json:"auction_end"`
+}
+
+func (q *Queries) GetAllProducts(ctx context.Context) ([]GetAllProductsRow, error) {
+	rows, err := q.db.Query(ctx, getAllProducts)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []GetAllProductsRow
+	for rows.Next() {
+		var i GetAllProductsRow
+		if err := rows.Scan(
+			&i.SellerID,
+			&i.ProductName,
+			&i.Description,
+			&i.Baseprice,
+			&i.AuctionEnd,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+const getProductById = `-- name: GetProductById :one
+SELECT id, seller_id, product_name, description, baseprice, auction_end, is_sold, created_at, updated_at FROM products 
+WHERE id = $1
+`
+
+func (q *Queries) GetProductById(ctx context.Context, id uuid.UUID) (Product, error) {
+	row := q.db.QueryRow(ctx, getProductById, id)
+	var i Product
+	err := row.Scan(
+		&i.ID,
+		&i.SellerID,
+		&i.ProductName,
+		&i.Description,
+		&i.Baseprice,
+		&i.AuctionEnd,
+		&i.IsSold,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+	)
+	return i, err
+}
--- internal/store/pgstore/queries/products.sql
+++ internal/store/pgstore/queries/products.sql
@@ -4,3 +4,17 @@ INSERT INTO products(
     baseprice, auction_end
 ) VALUES ($1, $2, $3, $4, $5)
 RETURNING id;
+
+-- name: GetAllProducts :many
+SELECT 
+    seller_id,
+    product_name, 
+    description, 
+    baseprice,
+    auction_end
+FROM 
+    products;
+
+-- name: GetProductById :one
+SELECT * FROM products 
+WHERE id = $1;
